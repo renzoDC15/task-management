1f4cd08d223e007b12fda1bfeb2a51c40e07d703
RDC15 <34361260+renzoDC15@users.noreply.github.com>
2025-07-01 06:54:16 +0800

    .

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
+
+
+Route::post('/register', RegisterController::class);
+Route::post('/login', LoginController::class);
 
 Route::middleware('auth:sanctum')->group(function () {
 });
